af0f32b4350b7770c2aa81ff98534899963c3527
svonw <88006181+svonw@users.noreply.github.com>
2024-02-28 19:04:06 +0100

    last upload

--- app.js
+++ app.js
@@ -4,4 +4,25 @@
   angular
     .module('LunchCheck', [])
     .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope) {
+    $scope.food = '';
+    $scope.message = '';
+
+    $scope.CheckFood = function () {
+      if ($scope.food === '') {
+        $scope.message = 'Please enter data first';
+        return;
+      }
+      $scope.items = $scope.food.split(',');
+      if ($scope.items.length <= 3) {
+        $scope.message = 'Enjoy!';
+      } else {
+        $scope.message = 'Too much!';
+      }
+      console.log($scope.message);
+    };
+  }
 })();
